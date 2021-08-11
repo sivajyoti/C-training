d8745c88a1711bd9654ee1eef4b7a1a22522b115
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-11 16:22:28 +0530

    Create variables.cpp

---
+++ Project01/variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+//{
+//    int count = 15;
+//    int data = count + 1;
+//   
+//    std::cout << data << std :: endl; 
+//    return 0;
+//}
+
+{
+	int value = 43;
+	int number = value+5;
+		value = 54;
+		std::cout << number << std :: endl;
+		return 0;
+}
