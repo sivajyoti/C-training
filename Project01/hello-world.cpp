e87e2534f08913ba5e95bc97525a2544579b18ce
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-11 16:08:29 +0530

    Create hello-world.cpp

---
+++ Project01/hello-world.cpp
@@ -0,0 +1,15 @@
+//Using name space by specifing the one standard object (cout), if wanted to use any other object need to use std
+
+#include <iostream>
+using std::cout;
+////int main()
+////{
+////    cout << "Hello World!\n";
+////    return 0;
+//}
+
+int main()
+{
+    cout << "New project using specific standard obj" << std :: endl;
+    return 0;
+}
