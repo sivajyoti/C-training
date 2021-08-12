3d9ee2ff57d24e69ed3fccb03a866d44d368583c
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-12 19:45:29 +0530

    Update using functions and void.cpp

--- Project01/using functions and void.cpp
+++ Project01/using functions and void.cpp
@@ -6,55 +6,24 @@
 using std::cin;
 using std::cout;
 
-//double power(double base, int exponent)
-//{
-//	return 1.0; //out will display 1,
-//}
-//int main()
-//{
-//		int base, exponet;
-//		cout << "What is the base? : ";
-//		cin >> base;
-//		cout << "What is the exponent? : ";
-//		cin >> exponet;
-//		double mypower = power (base, exponet);
-//		cout << mypower << std::endl;
-//		return 0;
-//	
-//}
-
-
-
-//using void
-#include <iostream>
-#include <cmath>
-
-using std::cin;
-using std::cout;
 double power(double base, int exponent)
 {
-	double result = 1;
-	for (int i = 0; i <exponent; i++)
-	{
-		result = result * base;
-	}
-	return result;
+	return 1.0; //out will display 1,
 }
-void  print_pow (double base, int exponent)
-	{
-	  double mypower = power(base, exponent);
-	  cout << base << " raised to the " << exponent << " power is " << mypower << std::endl;
-  }
-  int main()
-  {
-	  double base;
-	  int exponent;
-	  cout << "What is the base? : ";
-	  cin >> base;
-	  cout << "What is the exponent? : ";
-	  cin >> exponent;
-	  print_pow(base, exponent);
-	  print_pow(2, 4);
-	  return 0;
-  }
+int main()
+{
+	int base, exponet;
+	cout << "What is the base? : ";
+	cin >> base;
+	cout << "What is the exponent? : ";
+	cin >> exponet;
+	double mypower = power (base, exponet);
+	cout << mypower << std::endl;
+	return 0;
+	
+}
+
+
+
+
 
