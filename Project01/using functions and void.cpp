86c6b557c783057388e01b3f387b23c81cf13075
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-12 19:43:31 +0530

    Update using functions and void.cpp

--- Project01/using functions and void.cpp
+++ Project01/using functions and void.cpp
@@ -23,28 +23,7 @@ using std::cout;
 //	
 //}
 
-double power(double base, int exponent)
-{
-	double result = 1;
-	for (int i = 0; i <exponent; i++)
-	{
-		result = result * base;
-	}
-	return result;
-}
 
-int main()
-{
-		int base, exponet;
-		cout << "What is the base? : ";
-		cin >> base;
-		cout << "What is the exponent? : ";
-		cin >> exponet;
-		double mypower = power (base, exponet);
-		cout << mypower << std::endl;
-		return 0;
-	
-}
 
 //using void
 #include <iostream>
