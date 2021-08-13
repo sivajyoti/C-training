85bf7b766dbe71935d09e364d37e3ffa5e8df9be
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-13 11:28:43 +0530

    Create escape-sequence.cpp

---
+++ Project01/escape-sequence.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello\tjack " << std::endl;  //tab
+	std::cout << " Hello\bsara " << std::endl; //backspace
+	std::cout << " Hello\nvirat" << std::endl; //new line
+  std::cout << " Hello\vlol" << std::endl;
+	std::cout << "Hello\ahere" << std::endl;
+  std::cout << "Hello \"I am here\" " << std::endl;
+}
