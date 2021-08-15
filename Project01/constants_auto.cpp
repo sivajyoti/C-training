02af85f4482d1b7d46af28d2c581f4270eb0ffbe
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-15 13:08:11 +0530

    Create constants_auto.cpp

---
+++ Project01/constants_auto.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using std::cout;
+int main()
+{
+	auto a = 5U;
+	auto b = 4.7F;
+	auto c = 5.678L;
+	auto d = 5.0;
+	cout << a << std::endl;
+	cout << b << std::endl;
+	cout << c << std::endl;
+	cout << d << std::endl;
+}
