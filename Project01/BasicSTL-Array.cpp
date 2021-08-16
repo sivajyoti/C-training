bacf4b6be277a8b992e806c941a0ae09b5d0f349
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-16 19:21:39 +0530

    Create BasicSTL-Array.cpp

---
+++ Project01/BasicSTL-Array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+int main()
+{
+	std::array <int, 25> value = { 12, 10, 45,15, 78 };
+
+	std::cout << value[3] << std::endl; //will print the 3rd index value as output
+}
