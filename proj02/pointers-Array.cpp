9eb4360671c4b56f131a77513068588c2c5dc7cb
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-24 18:47:03 +0530

    Update pointers-Array.cpp

--- proj02/pointers-Array.cpp
+++ proj02/pointers-Array.cpp
@@ -14,3 +14,21 @@ int main()
 	std::cout << " The address of A is " << &A[0] + 1 << std::endl;
 	std::cout << "The value of first position is" << A[0]+1 << std::endl;
 }
+
+using loops
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int A[] = { 2, 4, 5, 8, 1 };
+	int i;
+	for(int i = 0; i <=5; i++)
+	{
+		std::cout << &A[i] << std::endl;
+		std::cout << A + i << std::endl;
+		std::cout << A[i] << std::endl;
+		std::cout << *(A + i) << std::endl;
+	}
+}
