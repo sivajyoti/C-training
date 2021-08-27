d27ed316449eaea5148b612f9a8f0464efa64cca
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-27 11:48:49 +0530

    Update fun-pointer-callback.cpp

--- proj02/fun-pointer-callback.cpp
+++ proj02/fun-pointer-callback.cpp
@@ -67,3 +67,50 @@ int main()
 	}
 }
 
+
+//Using signe dnumber
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int absolute_compare(int a, int b)
+{
+	if (abs(a) > abs(b)) return 1;
+	return -1;
+}
+
+int compare(int a, int b)
+{
+	if (a > b) return -1;
+	else return 1;
+}
+
+
+void Bubblesort(int A[], int n, int(*compare) (int, int))
+{
+	int i, j, temp;
+	for(i=0; i<n; i++)
+		for (j = 0; j< n - 1; j++)
+		{
+			if (compare(A[j], A[j + 1]) >0)
+			{
+				temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = temp;
+			}
+		}
+}
+
+int main()
+{
+	int i;
+	int A[] = { 3, -51, 4, 1, -28, 7 };
+	Bubblesort(A,6,absolute_compare);
+	for (i = 0; i < 6; i++)
+	{
+		std::cout << A[i] << std::endl;
+	}
+}
+
+
