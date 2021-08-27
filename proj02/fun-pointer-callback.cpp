1761d50334a0d2175a9acdbc1bc4fcac00b7f4a3
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-27 11:31:57 +0530

    Create fun-pointer-callback.cpp

---
+++ proj02/fun-pointer-callback.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stdio.h>
+
+void Bubblesort(int A[], int n)
+{
+	int i, j, temp;
+	for(i=0; i<n; i++)
+		for (j = 0; j< n - 1; j++)
+		{
+			if (A[j] > A[j + 1])
+			{
+				temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = temp;
+			}
+		}
+}
+
+int main()
+{
+	int i;
+	int A[] = { 3, 5, 4, 1, 2, 7 };
+	Bubblesort(A,6);
+	for (i = 0; i < 6; i++)
+	{
+		std::cout << A[i] << std::endl;
+	}
+}
+
+//Now,we'll use function to call
+#include <iostream>
+#include <stdio.h>
+
+int compare(int a, int b)
+{
+	if (a > b) return -1;
+	else return 1;
+}
+
+
+void Bubblesort(int A[], int n, int(*compare) (int, int))
+{
+	int i, j, temp;
+	for(i=0; i<n; i++)
+		for (j = 0; j< n - 1; j++)
+		{
+			if (compare(A[j], A[j + 1]) >0)
+			{
+				temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = temp;
+			}
+		}
+}
+
+int main()
+{
+	int i;
+	int A[] = { 3, 5, 4, 1, 2, 7 };
+	Bubblesort(A,6,compare);
+	for (i = 0; i < 6; i++)
+	{
+		std::cout << A[i] << std::endl;
+	}
+}
+
