374d4a186576de3d6ff5c4aae96f83440eaacb5c
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-08-31 09:23:50 +0530

    Create For-llop.cpp

---
+++ Project01/Range-loop/For-llop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+int main()
+{
+	int data[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+	for (int i = 0; i < 8; i++)
+	{
+		std::cout << data[i] << std::endl;
+	}
+	std::cout << std::endl;
+}
