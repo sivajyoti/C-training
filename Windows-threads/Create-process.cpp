25f9372d9f5f39ac4cdd1d21d6a8ec0fcda6f9b9
sivajyoti <88774639+sivajyoti@users.noreply.github.com>
2021-09-15 14:05:16 +0530

    Update Create-process.cpp

--- Windows-threads/Create-process.cpp
+++ Windows-threads/Create-process.cpp
@@ -1,31 +1,30 @@
 //Create Process flow
 
-#include <windows.h>
+#include <Windows.h>
+#include <iostream>
 
-using namespace std;
+int main()
 
-int _tmain(int argc, _TCHAR* argv[])
 {
-
 	PROCESS_INFORMATION pi;
 	STARTUPINFO si = { sizeof(si) };
-
-	TCHAR name[] = _T(" notepad ");
+	TCHAR name[] = (L"notepad");
 	BOOL success = CreateProcess(nullptr, name, nullptr, nullptr, FALSE, 0, nullptr, nullptr, &si, &pi);
 
 	if (success)
 	{
-		cout << "PID: " << pi.dwProcessId << endl;
-		cout << " hi.process " << pi.hProcess;
+		std::cout << "PID: " << pi.dwProcessId << std::endl;
+		std::cout << " hProcess: " << pi.hProcess;
 
 		WaitForSingleObject(pi.hProcess, INFINITE);
 		DWORD code;
 		GetExitCodeProcess(pi.hProcess, &code);
-		cout << " Notepad has exited. Exit code=" << code << endl;
+
+		std::cout << " Notepad has exited. Exit code=" << code << std::endl;
 	}
 	else
 	{
-		cout << GetLastError() << endl;
+		std::cout << GetLastError() << std::endl;
 	}
 	return 0;
 }
